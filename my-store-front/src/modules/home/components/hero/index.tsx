c1ad1fc40981c1a64c9c81cc35a7b58b3f816474
Hiresh Yadav <103920023+hiresh1503@users.noreply.github.com>
2022-10-26 00:31:42 +0530

    Add files via upload

---
+++ my-store-front/src/modules/home/components/hero/index.tsx
@@ -0,0 +1,32 @@
+import UnderlineLink from "@modules/common/components/underline-link"
+import Image from "next/image"
+
+const Hero = () => {
+  return (
+    <div className="h-[90vh] w-full relative">
+      <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
+        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
+          Winter time-styles are finally here.
+        </h1>
+        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
+          This year, our new winter collection will shelter you from the harsh
+          elements of a world with a luxury collection of wrist-watches.
+        </p>
+        <UnderlineLink href="/store">Explore products</UnderlineLink>
+      </div>
+      <Image
+        src="/hero.jpg"
+        layout="fill"
+        loading="eager"
+        priority={true}
+        quality={90}
+        objectFit="cover"
+        alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
+        className="absolute inset-0"
+        draggable="false"
+      />
+    </div>
+  )
+}
+
+export default Hero
